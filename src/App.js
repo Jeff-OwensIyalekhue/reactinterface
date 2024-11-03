e441e768c0d91806050d1473a449cd169150d57b
Jeff-OwensIyalekhue <j.oiyalekhue@hotmail.de>
2024-11-03 18:14:02 +0100

    create search component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import { GiConsoleController } from "react-icons/gi";
+import Search from "./components/Search";
 
 function App() {
   return (
-    <div
-      className="App container
-  mx-auto mt-3 font-thin"
-    >
+    <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
-        <GiConsoleController
-          className="inline-block
-    text-red-400 align-top"
-        />
+        <GiConsoleController className="inline-block text-red-400 align-top" />
         Repos
       </h1>
+      <Search />
     </div>
   );
 }
