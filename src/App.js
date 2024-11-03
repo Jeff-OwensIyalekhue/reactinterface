12a1626d8c0e41653e2532ec0dddeaf9131cc7bf
Jeff-OwensIyalekhue <j.oiyalekhue@hotmail.de>
2024-11-03 17:49:56 +0100

    adding tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import { GiConsoleController } from "react-icons/gi";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        <GiConsoleController />
+    <div
+      className="App container
+  mx-auto mt-3 font-thin"
+    >
+      <h1 className="text-5xl">
+        <GiConsoleController
+          className="inline-block
+    text-red-400 align-top"
+        />
         Repos
       </h1>
     </div>
