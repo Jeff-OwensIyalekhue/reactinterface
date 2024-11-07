8a7b96971d731778707fe78bfc29a2cdfae3617b
Jeff-OwensIyalekhue <j.oiyalekhue@hotmail.de>
2024-11-07 16:26:21 +0100

    adding display of appointment info, deleting appointment infos, filter appointment info list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
+import { useState, useEffect, useCallback } from "react";
 import { GiConsoleController } from "react-icons/gi";
 import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
+import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+
+  const filteredAppointments = appointmentList.filter((item) => {
+    return (
+      item.petName.toLowerCase().includes(query.toLowerCase()) ||
+      item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+      item.aptNotes.toLowerCase().includes(query.toLowerCase())
+    );
+  });
+
+  const fetchData = useCallback(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setAppointmentList(data);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
         <GiConsoleController className="inline-block text-red-400 align-top" />
         Repos
       </h1>
-      <Search />
+      <AddAppointment />
+      <Search query={query} onQueryChange={(myQuery) => setQuery(myQuery)} />
+
+      <ul className="divide-y divide-grey-200">
+        {filteredAppointments.map((appointmentItem) => (
+          <AppointmentInfo
+            key={appointmentItem.id}
+            appointment={appointmentItem}
+            onDeleteAppointment={(appointmentId) =>
+              setAppointmentList(
+                appointmentList.filter(
+                  (appointment) => appointment.id !== appointmentId
+                )
+              )
+            }
+          />
+        ))}
+      </ul>
     </div>
   );
 }
